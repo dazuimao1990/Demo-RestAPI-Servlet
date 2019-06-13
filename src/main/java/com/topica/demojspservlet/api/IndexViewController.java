f042e82688bc52e237b4950a11b9a4d1734ec59b
barnett <576501057@qq.com>
2019-06-13 18:23:43 +0800

    change index

--- src/main/java/com/topica/demojspservlet/api/IndexViewController.java
+++ src/main/java/com/topica/demojspservlet/api/IndexViewController.java
@@ -1,13 +1,20 @@
 package com.topica.demojspservlet.api;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.topica.demojspservlet.model.Todo;
 import com.topica.demojspservlet.service.TodoService;
 import com.topica.demojspservlet.service.impl.TodoServiceImpl;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,9 +28,42 @@ public class IndexViewController extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Todo> list = todoService.getAll();
-        System.out.println(Arrays.deepToString(list.toArray()));
-        req.setAttribute("todos", list);
-        req.getRequestDispatcher("TodoCRUD.jsp").forward(req, resp);
+        String address = "http://todos/api/todos";
+        URL url = new URL(address);
+        URLConnection conn = url.openConnection();
+        HttpURLConnection httpURLConnection = (HttpURLConnection) conn;
+        int code = httpURLConnection.getResponseCode();
+        if (code == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+            in.close();
+            JSONObject json = JSONObject.parseObject(result.toString());// 把String对象转为JSONObject对象
+            List<Todo> todoList = new ArrayList<>();
+            JSONArray jsonarray = json.getJSONArray("todoList");
+            for (int i = 0; i < jsonarray.size(); i++) {
+                Todo todo = new Todo();
+                JSONObject jo = jsonarray.getJSONObject(i);
+                todo.setId(jo.getLong("id"));
+                todo.setName(jo.getString("name"));
+                todo.setDescription(jo.getString("description"));
+                todoList.add(todo);
+            }
+            req.setAttribute("todos", todoList);
+        } else {
+            req.setAttribute("todos", null);
+        }
+        if (code == 429) {
+            req.setAttribute("errorMsg", "后端服务限流");
+        }
+        if (code == 503) {
+            if (httpURLConnection.getHeaderField("x-envoy-overloaded") != null) {
+                req.setAttribute("errorMsg", "后端服务断路");
+            }
+        }
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,58 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>CRUD DEMO</title>
+</head>
+
+<body>
+<h2 style="text-align: center; color: blue">CRUD DEMO</h2>
+<hr>
+<h3 style="text-align: center; color: red;">${errors}</h3>
+<form id="frmTodo" name="frmTodo" method="post" action="todo">
+    <table border="0" align="center" cellpadding="5" cellspacing="0">
+        <tr>
+            <td>Todo Id</td>
+            <td><input readonly type="text" name="id" value="${todo.Id}"/></td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><input size="44" type="text" name="name" value="${todo.Name}" /></td>
+        </tr>
+        <tr>
+            <td>Description</td>
+            <td><textarea rows="2" cols="44" name="description">${todo.Description}</textarea></td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>
+                <input type="submit" name="btnInsert" id="btnInsert" value="Insert" />
+                <input type="submit" name="btnUpdate" id="btnUpdate" value="Update" />
+                <input type="submit" name="btnDelete" id="btnDelete" value="Delete" />
+            </td>
+        </tr>
+    </table>
+    <hr/>
+</form>
+<table width="100%" border="1" align="center" cellpadding="5" cellspacing="0">
+    <tr bgcolor="lightgray">
+        <th>Id</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>&nbsp;</th>
+    </tr>
+    <c:forEach var="todo" items="${todos}">
+        <tr>
+            <td>${todo.getId()}</td>
+            <td>${todo.getName()}</td>
+            <td>${todo.getDescription()}</td>
+            <td><a href="?lnkEdit&txtId=${todo.getId()}">Edit</a></td>
+        </tr>
+    </c:forEach>
+</table>
+<div>${errorMsg}</div>
+</body>
+</html>
