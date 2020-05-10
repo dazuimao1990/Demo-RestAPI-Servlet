41688fd92bbb669f5e9c201d797e529aa13ff253
dazuimao1990 <33855684+dazuimao1990@users.noreply.github.com>
2020-05-10 18:43:00 +0800

    Update run.sh
    膈应人的 空格

--- run.sh
+++ run.sh
@@ -5,10 +5,7 @@ if [[ $ENABLE_APM == "true" ]];then
  PINPOINT_AGETN_VERSION=1.7.2
  PINPOINT_AGENT_PATH=/app/pinpoint
 #将 pinpoint agent 启动参数加⼊到 $JAVA_OPTS 中 
-export JAVA_OPTS="$JAVA_OPTS -javaagent:$ 
-{PINPOINT_AGENT_PATH}/pinpoint-bootstrap-${PINPOINT_AGETN_VERSION}-
-SNAPSHOT.jar -Dpinpoint.agentId=${AGENT_ID:-${SERVICE_ID:0:10}} -Dpinpoint.applicationName=${APP_NAME:-${SERVICE_NAME:-
-$HOSTNAME}}"
+export JAVA_OPTS="$JAVA_OPTS -javaagent:${PINPOINT_AGENT_PATH}/pinpoint-bootstrap-${PINPOINT_AGETN_VERSION}-SNAPSHOT.jar -Dpinpoint.agentId=${AGENT_ID:-${SERVICE_ID:0:10}} -Dpinpoint.applicationName=${APP_NAME:-${SERVICE_NAME:-$HOSTNAME}}"
 fi
 PORT=${PORT:-5000}
 sleep ${PAUSE:-0}
